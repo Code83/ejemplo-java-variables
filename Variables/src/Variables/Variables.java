4a427a3878288e99048f57ed0a2831443ab9133a
gaguajardo <gaguajardo@sodimac.cl>
2020-04-17 13:15:11 -0400

    trabajando tipos de variables

---
+++ Variables/src/Variables/Variables.java
@@ -0,0 +1,13 @@
+package Variables;
+
+public class Variables {
+
+	public static void main(String arg[]) {
+		
+		int var = 5;
+		char var2 ='C';
+		
+		System.out.println("Las variables son: " + var + var2);
+	}
+	
+}
